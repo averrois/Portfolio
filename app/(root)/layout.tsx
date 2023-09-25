bf12bf0946aa50f21e44ab7d0e94441ce0c566e2
Yassin-Aru <jocarhisouka@gmail.com>
2023-09-25 20:55:05 +0100

    Updating Main Root Layouts ...

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,13 @@
+import Footer from '@/components/ui/Footer'
+import Navbar from '@/components/ui/Navbar'
 import React from 'react'
 
 function layout({children} : {children: React.ReactNode}) {
   return (
     <>
-      NavBar
+      <Navbar />
       {children}
-      Footer
-
+      <Footer />
     </>
   )  
 }
