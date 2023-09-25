6bca03c963884c25e37bf67a19e85796e4f85949
Yassin-Aru <jocarhisouka@gmail.com>
2023-09-25 21:03:50 +0100

    Creating Navbar Comp ...

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white'>
+      <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px'>
+        <Link  
+      </div>
+    </nav>
   )
 }
 
