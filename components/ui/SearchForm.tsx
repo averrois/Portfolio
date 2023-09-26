1ecf2326133acc2e2a0b7f84a6ce4dcdf059495b
Yassin-Aru <jocarhisouka@gmail.com>
2023-09-26 12:55:20 +0100

    Adding Search Icon ...

--- components/ui/SearchForm.tsx
+++ components/ui/SearchForm.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 function SearchForm() {
-  return (
-    <div>SearchForm</div>
-  )
+    return (
+        <form action="">
+            <label htmlFor="">
+                <Image
+                    src='/magnifying-glass.svg'
+                    className='absolute left-8'
+                    width={32}
+                    height={32}
+                    alt='Search Icon'
+                />
+            </label>
+        </form>
+    )
 }
 
 export default SearchForm
