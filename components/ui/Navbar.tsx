e4f011e75a6ad909cbf91fc86afc00303c90f982
Yassin-Aru <jocarhisouka@gmail.com>
2023-09-26 07:33:33 +0100

    Adding logo to Navbar...

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,7 +6,14 @@ function Navbar() {
   return (
     <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white'>
       <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px'>
-        <Link  
+        <Link href='/' >
+          <Image 
+            src='/main_logo.svg'
+            width={55}
+            height={40}
+            alt='logo'
+          />
+       </Link> 
       </div>
     </nav>
   )
