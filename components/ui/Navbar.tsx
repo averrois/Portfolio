288f183a23462885b311e7d53bf1c8d3bfbd5ee7
Yassin-Aru <jocarhisouka@gmail.com>
2023-09-26 08:27:40 +0100

    Adding Banner ...

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white'>
-      <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px'>
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 bg-black-100 py-7 text-white">
+      <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href='/' >
           <Image
             src='/main_logo.svg'
