6c78b8e2a1298e29502886eb59b230699315a9a6
Yassin-Aru <jocarhisouka@gmail.com>
2023-09-26 07:57:21 +0100

    Adding NavbarMenu items ...

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -7,20 +7,46 @@ function Navbar() {
     <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white'>
       <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px'>
         <Link href='/' >
-          <Image 
+          <Image
             src='/main_logo.svg'
             width={55}
             height={40}
             alt='logo'
           />
         </Link>
-        <Image 
+        <Image
           src='/hamburger-menu.svg'
           width={30}
           height={30}
           alt='Navbar Menu'
           className='block md:hidden'
         />
+        <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-100'>
+          <li className='body-text text-gradient_blue-purple !font-semibold	'>
+            <Link
+              href='https://www.github.com/Yassin-Aru'
+              target='_blank'
+            >
+              Github
+            </Link>
+          </li>
+          <li className='body-text text-gradient_blue-purple !font-semibold	'>
+            <Link
+              href='https://www.linkedin.com/in/yassin-aru/'
+              target='_blank'
+            >
+              Linkedin
+            </Link>
+          </li>
+          <li className='body-text text-gradient_blue-purple !font-semibold	'>
+            <Link
+              href='https://www.figma.com/@yassin_aru'
+              target='_blank'
+            >
+              Figma
+            </Link>
+          </li>
+        </ul>
 
       </div>
     </nav>
